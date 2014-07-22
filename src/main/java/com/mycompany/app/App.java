09e7f2185a8da45f8c2b5c1c61b47d40cf148451
Grzegorz Aperliński <grzegorzaperlinski@MacBook-Pro-Grzegorz.local>
2014-07-22 17:01:16 +0200

    lesson 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,19 +8,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        System.out.println( "Hello World!" );
         Calculator calculator = new Calculator(2.2f);
-        Calculator calculator2 = new Calculator(3f);
-        calculator.name = "calc";
-        //System.out.println(Calculator.name);
-        calculator.add(5f);
-        System.out.println(calculator2.name);
-        System.out.println(calculator.getTotal());
-        calculator2.subtract(2f);
-        System.out.println(calculator2.getTotal());
+//        Calculator calculator2 = new Calculator(3f);
+//        calculator.name = "calc";
+//        //System.out.println(Calculator.name);
+//        calculator.add(5f);
+//        System.out.println(calculator2.name);
+//        System.out.println(calculator.getTotal());
+//        calculator2.subtract(2f);
+//        System.out.println(calculator2.getTotal());
+//
+//        calculator.add(5f).subtract(1f).add(2.5f);
+//        System.out.println(calculator.getTotal());
 
-        calculator.add(5f).subtract(1f).add(2.5f);
-        System.out.println(calculator.getTotal());
-
+        for (int i = 0; i < 10; i++) {
+            calculator.add(i);
+            System.out.println(calculator.getTotal());
+        }
     }
 }
