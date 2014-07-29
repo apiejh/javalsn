216b4887b2fe74ee24d0b1ff25c5c2342aecdfc4
Grzegorz Aperliński <grzegorzaperlinski@MacBook-Pro-Grzegorz.local>
2014-07-29 17:06:47 +0200

    lesson 3

---
+++ src/test/java/com/mycompany/app/lesson3/LifeTest.java
@@ -0,0 +1,12 @@
+package com.mycompany.app.lesson3;
+
+/**
+ * Created by grzegorzaperlinski on 29.07.2014.
+ */
+public class LifeTest {
+    /*
+    constructor tablica person, metoda age postarza o tyle ile jest lat w parametrze, jesli "j" postarza 2x szybciej
+    getter pobierajacy persony
+    getter pobierajacy sume lat
+    */
+}
