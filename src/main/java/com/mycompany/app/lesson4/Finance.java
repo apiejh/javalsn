f4da81346b39dfb53779a8fa1a3975ab92ada445
Grzegorz Aperliński <grzegorzaperlinski@MacBook-Pro-Grzegorz.local>
2014-08-26 16:26:08 +0200

    lesson 5 usuniecie duplikacji kodu metody payDailyWage

--- src/main/java/com/mycompany/app/lesson4/Finance.java
+++ src/main/java/com/mycompany/app/lesson4/Finance.java
@@ -4,12 +4,8 @@ package com.mycompany.app.lesson4;
  * Created by grzegorzaperlinski on 19.08.2014.
  */
 public class Finance {
-    public void payDailyWage(Miner miner) {
-        miner.receivePayment(8);
-    }
-
-    public void payDailyWage(Accountant accountant) {
-        accountant.receivePayment(8);
+    public void payDailyWage(Worker worker) {
+        worker.receivePayment(8);
     }
 
     public void giveBonus(Accountant accountant) {
